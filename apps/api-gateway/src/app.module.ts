9335c2dbc20761e208568a9ce3b84fae75fe9cc9
Frey <duongpham321dz@gmail.com>
2026-01-27 15:25:25 +0700

    feat: refactor code

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,29 +1,10 @@
-import { MICROSERVICE_CLIENTS } from 'apps/api-gateway/constant';
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { BookingsModule } from './bookings/bookings.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: MICROSERVICE_CLIENTS.BOOKINGS_SERVICE,
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'],
-          queue: 'bookings_queue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-    UsersModule,
-    BookingsModule,
-    AuthModule,
-  ],
+  imports: [UsersModule, BookingsModule, AuthModule],
   controllers: [],
   providers: [],
 })
